95b61dbc4cafcf51df025ad253c8a9ee00b20ca4
Fauzyah Hadirahma <97279376+fauzyahadirahma@users.noreply.github.com>
2022-07-20 23:06:58 +0700

    Kuis Coding : Object

---
+++ main3.js
@@ -0,0 +1,44 @@
+/**
+ * TODO
+ * 1. Buatlah variabel dengan nama restaurant yang bertipe object dengan ketentuan berikut:
+ *    - Memiliki properti bernama "name"
+ *       - Bertipe data string
+ *       - Bernilai apa pun, asalkan tidak string kosong atau null.
+ *    - Memiliki properti bernama "city"
+ *       - Bertipe data string
+ *       - Bernilai apa pun, asalkan tidak string kosong atau null.
+ *    - Memiliki properti "favorite drink"
+ *       - Bertipe data string
+ *       - Bernilai apa pun, asalkan tidak string kosong atau null.
+ *    - Memiliki properti "favorite food"
+ *       - Bertipe data string
+ *       - Bernilai apa pun, asalkan tidak string kosong atau null.
+ *    - Memiliki properti "isVegan"
+ *       - Bertipe data boolean
+ *       - Bernilai boolean apa pun.
+ *
+ * 2. Buatlah variabel bernama name.
+ *    Kemudian isi dengan nilai name dari properti object restaurant
+ * 3. Buatlah variabel bernama favoriteDrink.
+ *    Kemudian isi dengan nilai "favorite drink" dari properti object restaurant
+ */
+
+
+// TODO
+const restaurant = {
+    name : "Pizza Hut",
+    city : "Bandung",
+    "favorite food" : "Pizza Tuna Melt",
+    "favorite drink": "Chocolate Milkshake",
+    isVegan : false,
+};
+
+let name = restaurant.name;
+let favoriteDrink = restaurant["favorite drink"];
+
+console.log(`Minuman favoritku adalah ${favoriteDrink} dari ${name}`);
+
+/**
+ * Jangan hapus kode di bawah ini
+ */
+module.exports = { restaurant, name, favoriteDrink };
